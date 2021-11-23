048b6bec151d4850a91459a6c6ed7bc6cf9acdfa
MMMeteor <38771622+MMMeteor@users.noreply.github.com>
2021-11-23 20:31:28 +0100

    23/11/2021

--- submissions/external_data/estimator.py
+++ submissions/external_data/estimator.py
@@ -8,6 +8,8 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.pipeline import make_pipeline
 from sklearn.compose import ColumnTransformer
 from sklearn.linear_model import Ridge
+from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.preprocessing import OrdinalEncoder
 
 
 def _encode_dates(X):
@@ -23,18 +25,18 @@ def _encode_dates(X):
     return X.drop(columns=["date"])
 
 
-def _merge_external_data(X):
+def _merge_external_data(data_train):
     file_path = Path(__file__).parent / 'external_data.csv'
     df_ext = pd.read_csv(file_path, parse_dates=['date'])
     
-    X = X.copy()
+    data_train = data_train.copy()
     # When using merge_asof left frame need to be sorted
-    X['orig_index'] = np.arange(X.shape[0])
-    X = pd.merge_asof(X.sort_values('date'), df_ext[['date', 't']].sort_values('date'), on='date')
+    data_train['orig_index'] = np.arange(data_train.shape[0])
+    data_train = pd.merge_asof(data_train.sort_values('date'), df_ext[['date', 'dd', 'ff', 't', 'td', 'u', 'vv', 'total_cases', 'new_deaths', 'is_workday', 'lockdown', 'curfew']].sort_values('date'), on='date')
     # Sort back to the original order
-    X = X.sort_values('orig_index')
-    del X['orig_index']
-    return X
+    data_train = data_train.sort_values('orig_index')
+    del data_train['orig_index']
+    return data_train
 
 
 
@@ -42,17 +44,32 @@ def get_estimator():
     date_encoder = FunctionTransformer(_encode_dates)
     date_cols = ['year', 'month', 'day', 'weekday', 'hour']
 
-    categorical_encoder = OneHotEncoder(handle_unknown="ignore")
+    categorical_encoder = OrdinalEncoder(handle_unknown='use_encoded_value', unknown_value=-1)
     categorical_cols = ["counter_name", "site_name"]
 
-    preprocessor = ColumnTransformer(
-        [
-            ("date", OneHotEncoder(handle_unknown="ignore"), date_cols),
-            ("cat", categorical_encoder, categorical_cols),
-        ]
-    )
-    regressor = Ridge()
+    numeric_cols = ['dd', 'ff', 't', 'td', 'u', 'vv', 'total_cases', 'new_deaths', 'is_workday', 'lockdown', 'curfew']
 
-    pipe = make_pipeline(FunctionTransformer(_merge_external_data, validate=False), date_encoder, preprocessor, regressor)
+    preprocessor = ColumnTransformer([
+        ('date', "passthrough", date_cols),
+        ('cat', OrdinalEncoder(handle_unknown='use_encoded_value', unknown_value=-1), categorical_cols),
+        ('numeric', 'passthrough', numeric_cols)
+    ])
+
+    params = {
+        "n_estimators": 500,
+        "max_depth": 4,
+        "min_samples_split": 5,
+        "learning_rate": 0.01,
+        "loss": "squared_error",
+    }
+
+    regressor = GradientBoostingRegressor(**params)
+
+    pipe =  make_pipeline(
+        FunctionTransformer(_merge_external_data, validate=False),
+        date_encoder,
+        preprocessor,
+        regressor
+    )
 
     return pipe
